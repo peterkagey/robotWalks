63a1e671e52145d361a00131aba523aca0251f3f
Peter Kagey <peterkagey@gmail.com>
2021-04-10 10:58:09 -0700

    Add comment to secrets.py.

--- secrets.py
+++ secrets.py
@@ -1,3 +1,8 @@
+# Make your changes here.
+# Do not push to Github.
+# You can have git ignore this file with
+#   `git update-index --skip-worktree secrets.py`
+
 CONSUMER_KEY        = "consumer_key"
 CONSUMER_SECRET     = "consumer_secret"
 ACCESS_TOKEN        = "access_token"
